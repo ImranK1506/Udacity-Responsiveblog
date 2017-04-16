6750892e33e92d15069b61ee3205eda7315faacb
Imran Khan <m_imran_khan@me.com>
2017-04-16 17:19:41 +0200

    first commit

---
+++ js/concat.js
@@ -0,0 +1,99 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            name: 'small',
+            width: '30%',
+            suffix: '_small',
+            quality: 20
+          },{
+            name: 'large',
+            width: '50%',
+            suffix: '_large',
+            quality: 40
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* sass */
+    sass: {                             // Task
+      dist: {                           // Target
+        files: {                        // Dictionary of files
+        'main.css': 'main.scss',        // 'destination': 'source'
+        'widgets.css': 'widgets.scss'
+        }
+      }
+    },
+
+    /* concat */
+    concat: {
+        dist: {
+          files: {
+          'js/concat.js': '*.js'
+        }
+      }
+    },
+
+    /* Copy the "fixed" images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: 'images_src/fixed/*.{gif,jpg,png}',
+          dest: 'images/'
+        }]
+      },
+    },
+  });
+  
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', [
+    'clean',
+    'mkdir',
+    'copy',
+    'responsive_images',
+    'sass',
+    'concat'
+  ]);
+};
